cb226032931625507af1ff8d8b5fdbdd7df30371
JUNE <jjhttoz@naver.com>
2020-11-16 20:40:18 +0900

    홈 액티비티 추가

---
+++ samples/augmented_image_java/app/src/main/java/com/google/ar/core/examples/java/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.google.ar.core.examples.java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.ar.core.examples.java.augmentedimage.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
